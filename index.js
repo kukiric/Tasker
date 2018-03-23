67d3376894fdf2d612ea4a5836c57cc3f9b7e312
Ricardo Maes <ricmzn@gmail.com>
2018-03-23 12:03:09 -0300

    Re-ativada verificação de tipos

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-// Carrega as configurações da aplicação
-require("dotenv").config();
-// Busca os módulos a partir do diretório raiz
-require("app-module-path").addPath(__dirname);
-require("start/main");
+try {
+    // Carrega as configurações da aplicação
+    require("dotenv").config();
+    // Busca os módulos a partir do diretório raiz
+    require("app-module-path").addPath(__dirname);
+    require("start/main");
+}
+catch (err) {
+    console.error(`\nUncaught ${err.stack}`);
+}
