de21ecc495144aec9ae462fbf0b463e7334842ee
Ricardo Maes <ricmzn@gmail.com>
2018-04-11 09:15:39 -0300

    Revertida alteração do index.js para index.ts

---
+++ index.js
@@ -0,0 +1,18 @@
+function panic(err) {
+    console.error(`\nUncaught ${err.stack}`);
+    process.exit(1);
+}
+
+try {
+    // Trata erros incuráveis da aplicação
+    process.on("unhandledRejection", panic);
+    // Carrega as configurações da aplicação
+    require("dotenv").config();
+    // Busca os módulos a partir do diretório raiz
+    require("app-module-path").addPath(__dirname);
+    // Inicia a aplicação em TypeScript (requer ts-node ou scripts compilados)
+    require("start/main");
+}
+catch (err) {
+    panic(err);
+}
