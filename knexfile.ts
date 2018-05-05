6ee0ff6b89a81d8e610be2ec9bfb2ba1edc1447c
Ricardo Maes <ricmzn@gmail.com>
2018-05-05 15:34:26 -0300

    Adicionada configuração faltante de porta para o banco de dados

--- knexfile.ts
+++ knexfile.ts
@@ -6,6 +6,7 @@ module.exports = {
     connection: {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
+        port: process.env.DB_PORT,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_DATABASE,
         filename: process.env.DB_DATABASE
