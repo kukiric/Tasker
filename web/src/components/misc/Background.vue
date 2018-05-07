67f8bc90321a9a4956cd393f51bd9d15a10f4c30
Ricardo Maes <ricmzn@gmail.com>
2018-05-07 15:03:47 -0300

    Melhorado layout do background independete da resolução

--- web/src/components/misc/Background.vue
+++ web/src/components/misc/Background.vue
@@ -1,7 +1,5 @@
 <template>
-    <div class="lowres background">
-        <div class="background"></div>
-    </div>
+    <div class="background"></div>
 </template>
 <style scoped>
     .background {
@@ -10,14 +8,9 @@
         left: 0;
         width: 100%;
         height: 100%;
-        background-image: url("https://source.unsplash.com/1920x1080/daily?nature");   
+        background-image: url("https://source.unsplash.com/1920x1080/daily?nature");
         background-repeat: no-repeat;
         background-size: cover;
         z-index: -1000;
-    }
-    .lowres.background {
-        background-image: url("https://source.unsplash.com/192x108/daily?nature");   
-        position: relative;
-        z-index: -1100;
     }
 </style>
